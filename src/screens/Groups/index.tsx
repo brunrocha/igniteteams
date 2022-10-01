822a7d17e2aabd5d51c0a7631a43703993cc4388
Bruno Rocha <brunorocha-brs@outlook.com>
2022-10-01 11:41:13 -0300

    Adding conditional style using flatlist

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -28,7 +28,7 @@ export function Groups() {
             title={item} 
           />
         )}
-        
+        contentContainerStyle={ groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => (
           <ListEmpty message='Que tal cadastrar a primeira turma' />
         )}
