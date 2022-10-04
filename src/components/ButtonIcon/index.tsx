61b139e17b7f21ec87b6f13c685f8bbc823c4a1c
Bruno Rocha <brunorocha-brs@outlook.com>
2022-10-04 13:44:21 -0300

    Adding ButtonIcon component

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,23 @@
+import { TouchableOpacityProps } from "react-native"; 
+import { MaterialIcons } from '@expo/vector-icons';
+
+import { Container, Icon, ButtonIconTypeStyleProps } from './styles';
+
+type Props = TouchableOpacityProps & {
+  icon: keyof typeof MaterialIcons.glyphMap;
+  type?: ButtonIconTypeStyleProps;
+}
+
+export function ButtonIcon({ icon, type = 'PRIMARY', ...rest }: Props) {
+  return (
+    <Container 
+      type={type} 
+      {...rest}
+    >
+      <Icon 
+        name={icon} 
+        type={type}
+      />
+    </Container>
+  );
+}
