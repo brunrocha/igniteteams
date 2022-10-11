1050b333b80b02cc210b72ca8e93796baf18fe03
Bruno Rocha <brunorocha-brs@outlook.com>
2022-10-11 14:21:38 -0300

    Cleaning app file

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import { StatusBar } from 'react-native';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { Players } from '@screens/Players';
+import { Groups } from '@screens/Groups';
 import { Loading } from '@components/Loading';
 
 import theme from './src/theme';
 
 export default function App() {
+  // carregamento das fontes
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
@@ -17,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Players /> : <Loading /> }
+      { fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
   );
 }
