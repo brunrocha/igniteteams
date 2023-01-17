b48ae49ce28470a384e527cdd741a31a014b6286
Bruno Rocha <brunorocha-brs@outlook.com>
2023-01-17 15:57:26 -0300

    Adding navigate between screens

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
-import { Container, Logo, BackButton, BackIcon } from "./styles";
+import { useNavigation } from "@react-navigation/native";
 
+import { Container, Logo, BackButton, BackIcon } from "./styles";
 
 import logoImg from '@assets/logo.png';
 
@@ -8,11 +9,17 @@ type Props = {
 }
 
 export function Header({ showBackButton = false }: Props) {
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.navigate('groups');
+  }
+
   return (
     <Container>
       { 
         showBackButton && 
-        <BackButton>
+        <BackButton onPress={handleGoBack}>
           <BackIcon />
         </BackButton>
       }
