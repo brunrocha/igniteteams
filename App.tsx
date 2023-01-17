300e811bb81a2de0a933b762c453c6bbfb5ce24a
Bruno Rocha <brunorocha-brs@outlook.com>
2023-01-17 15:00:17 -0300

    Setting route file

--- App.tsx
+++ App.tsx
@@ -2,10 +2,11 @@ import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { Groups } from '@screens/Groups';
 import { Loading } from '@components/Loading';
 
 import theme from './src/theme';
+
+import { Routes } from './src/routes';
 
 export default function App() {
   // carregamento das fontes
@@ -18,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Groups /> : <Loading /> }
+      { fontsLoaded ? <Routes /> : <Loading /> }
     </ThemeProvider>
   );
 }
