47b2c5af3cf4193c9a5366f7fabb9666983937a5
Bruno Rocha <brunorocha-brs@outlook.com>
2023-03-25 10:35:27 -0300

    Incluindo tratamento de erro para novos grupos

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { AppError } from "@utils/appError";
 
 import { Container, Content, Icon } from "./styles";
 
@@ -9,6 +10,7 @@ import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 
 import { groupCreate } from "@storage/group/groupCreate";
+import { Alert } from "react-native";
 
 export function NewGroup() {
   const [group, setGroup] = useState('');
@@ -17,10 +19,20 @@ export function NewGroup() {
 
   async function handleNew(){
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert('Novo Grupo', 'Informe o nome da turma.');
+      }
+
       await groupCreate(group);
       navigation.navigate('players', { group });
+
     } catch (error) {
-      console.log(error);
+      if (error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message);
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo');
+        console.log(error);
+      }
     }
   }
 
